7db8dcf15276cf6d6c319af905a32ac1977339df
suciayu28 <suci24si@mahasiswa.pcr.ac.id>
2025-11-16 18:09:03 +0700

    perubahan seeder jenis_surat

--- database/seeders/JenisSuratSeeder.php
+++ database/seeders/JenisSuratSeeder.php
@@ -9,23 +9,44 @@ class JenisSuratSeeder extends Seeder
 {
     public function run(): void
     {
-        JenisSurat::create([
-            'kode' => 'SKTM',
-            'nama_jenis' => 'Surat Keterangan Tidak Mampu',
-            'syarat_json' => [
-                'KTP',
-                'KK',
-                'Surat Pengantar RT'
+        $data = [
+            [
+                'kode' => 'SKD',
+                'nama_jenis' => 'Surat Keterangan Domisili',
+                'syarat_json' => [
+                    'Fotokopi KTP',
+                    'Fotokopi KK'
+                ],
             ],
-        ]);
+            [
+                'kode' => 'SKU',
+                'nama_jenis' => 'Surat Keterangan Usaha',
+                'syarat_json' => [
+                    'Fotokopi KTP',
+                    'Surat Pengantar RT'
+                ],
+            ],
+            [
+                'kode' => 'SKTM',
+                'nama_jenis' => 'Surat Keterangan Tidak Mampu',
+                'syarat_json' => [
+                    'Fotokopi KTP',
+                    'Fotokopi KK'
+                ],
+            ],
+            [
+                'kode' => 'SKCK',
+                'nama_jenis' => 'Surat Pengantar SKCK',
+                'syarat_json' => [
+                    'Fotokopi KTP',
+                    'Fotokopi KK',
+                    'Pas Foto 4x6'
+                ],
+            ],
+        ];
 
-        JenisSurat::create([
-            'kode' => 'DOM',
-            'nama_jenis' => 'Surat Domisili',
-            'syarat_json' => [
-                'KTP',
-                'KK'
-            ],
-        ]);
+        foreach ($data as $item) {
+            JenisSurat::create($item);
+        }
     }
 }
